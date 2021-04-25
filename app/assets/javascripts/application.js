cf0bab7487be0db000a7923f728ede145a21f028
Felipe Kari <felipe.kari@hotmail.com>
2021-04-25 03:40:07 -0400

    fix nested form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require jquery_ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-//= require('./nested-forms/addFields')
-//= require('./nested-forms/removeFields')
